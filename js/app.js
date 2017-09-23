6e126098f482bed2c698e10341d4bccf00802f10
Yousef <vip.91091@hotmail.com>
2017-09-23 20:21:53 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,92 +1,53 @@
-// Enemies our player must avoid
-var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function (x, y, speed) {
 
-    var x = x;
+    this.sprite = 'images/enemy-bug.png';
+        var x = x;
     var y = y;
-    var speed = spped;
+    var speed = getRandomInt(500, 500);
     
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+};
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed * dt;
+    if (this.x > 550) {
+        this.x = -120;
+        this.speed = getRandomInt(500, 500);
+    }
+    if (player.x < this.x + 60 && player.x + 37 > this.x && player.y < this.y + 25 && 30 + player.y > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
+};
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+var Player = function () {
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Player.prototype.update = function (dt) {
     this.x += this.speed * dt;
-
-    
     if (this.x > 550) {
         this.x = -120;
-        this.speed = 120 + Math.floor(Math.random() * 550);
+        this.speed = getRandomInt(500, 500);
     }
-    
-        if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
+    if (player.x < this.x + 60 && player.x + 37 > this.x && player.y < this.y + 25 && 30 + player.y > this.y) {
         player.x = 200;
         player.y = 380;
     }
     
-};
-
-// Draw the enemy on the screen, required method for game
-
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-var Player = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    var x = x
-    var y = y
-    var speed = spped
     
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
